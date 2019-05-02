82d150783bb73a2d51cb8e9b6f96bac98665c845
Zach Parker <parkerz410@gmail.com>
2019-05-02 18:06:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,17 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b
 };
+
+function inc(a) {
+  return a++
+};
+
+function dec(a) {
+  return a--
+};
+
+function makeInt(a) {
+  return parseInt(a)
+};
+
+
